5562483ea06f1d36c22a9f3e2d923cd0f13d36a0
Shashank358 <srshahare22@gmail.com>
2020-06-20 16:48:54 +0530

    Added new file

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config()
 const cors = require('cors');
-const User  = require("./models/user");
 
-const mongodbUrl = process.env.MONGO_URI || 'mongodb://localhost:27017/Library'
+const mongodbUrl = process.env.MONGO_URI || 'mongodb+srv://Infinity:hackathon@123@cluster0-irokk.mongodb.net/Library?retryWrites=true&w=majority'
 mongoose.connect(mongodbUrl,
     {useNewUrlParser: true, useUnifiedTopology: true})
         .then(connect => {
@@ -24,13 +23,6 @@ app.use(cors());
 //Routes
 app.use('/users', require('./routes/users'));
 
-app.get('/allusers', (req, res, next) => {
-    User.find()
-        .then(result => {
-            res.json(result)
-        })
-})
-
 //Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
