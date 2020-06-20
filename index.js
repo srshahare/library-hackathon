9ccc104e71d3c8c523ebdeeb53aacdd2faaea2f1
Shashank358 <srshahare22@gmail.com>
2020-06-20 15:25:28 +0530

    new commit

--- index.js
+++ index.js
@@ -19,6 +19,4 @@ app.use('/users', require('./routes/users'));
 
 //Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT);
