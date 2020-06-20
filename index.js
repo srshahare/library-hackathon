c8ab51acc495b37291f092d165059e0d3041e0d3
Shashank358 <srshahare22@gmail.com>
2020-06-20 15:48:54 +0530

    new commit

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config()
+const cors = require('cors');
 
 const mongodbUrl = process.env.MONGO_URI || 'mongodb+srv://Infinity:hackathon123@cluster0-irokk.mongodb.net/Library'
-
-mongoose.connect(mongodbUrl,
-{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(connect => console.log('connected to mongodb..'))
-    .catch(e => console.log('could not connect to mongodb', e))
 
 const app = express();
 
 //Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(cors());
 
 //Routes
 app.use('/users', require('./routes/users'));
 
 //Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+mongoose.connect(mongodbUrl,
+    {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(connect => {
+            app.listen(PORT);
+        })
+        .catch(e => console.log('could not connect to mongodb', e))
